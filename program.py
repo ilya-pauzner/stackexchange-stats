4c3cfe85e4e0fe1d6d901d327064c96803b739f2
ilya-the-lamer <ilya-pauzner@yandex.ru>
2016-04-12 23:06:28 +0300

    Added comments processing
    Refactored a little bit

--- program.py
+++ program.py
@@ -4,11 +4,13 @@ import string
 def get_xml(name):
     return xml.etree.ElementTree.parse(name).getroot()
 
+
 def have_any(substrings, string):
     for elem in substrings:
         if elem in string:
             return True
     return False
+
 
 def parse_question(elem, user_id):
     if have_any(lotr, elem.attrib['Tags']):
@@ -16,9 +18,53 @@ def parse_question(elem, user_id):
     if have_any(harry, elem.attrib['Tags']):
         harry_users.add(user_id)    
     if have_any(starwars, elem.attrib['Tags']):
-        starwars_users.add(user_id)    
+        starwars_users.add(user_id)  
+        
+        
+def parse_comment(elem, user_id):
+    elem = posts_dict[elem.attrib['PostId']]
+    if elem.attrib['PostTypeId'] in ('1', '2'):
+        while 'ParentId' in elem.attrib:
+            elem = posts_dict[elem.attrib['ParentId']]
+        if elem.attrib['PostTypeId'] == '1':
+            parse_question(elem, user_id)
+            
 
-tags = get_xml('Tags.xml')
+def open_files_and_process_them():
+    global posts, posts_dict
+    posts = get_xml('Posts.xml')
+    posts_dict = dict()
+    for elem in posts:
+        posts_dict[elem.attrib['Id']] = elem    
+
+    global users, users_dict
+    users = get_xml('Users.xml')
+    users_dict = dict()
+    for elem in users:
+        users_dict[elem.attrib['Id']] = elem    
+    
+    global comments
+    comments = get_xml('Comments.xml')
+    
+    global tags
+    tags = get_xml('Tags.xml')
+    
+
+def print_output():
+    answer = open('output.html', 'w', encoding = 'utf-8')
+    print('<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">', file = answer)
+    print('<html><head><title> Stats </title></head><body><h3> Here are users who participated in Lord of the Rings and Harry Potter sections, but did not participate in the Star wars section </h3><br>', file = answer) 
+    print('<table border = "1px solid black">', file = answer)
+    print('<th> Name </th> <th> Id </th>', file = answer)    
+    for i in range(len(good_guys)):
+        print('<tr> <td>', good_guys[i][0], '</td> <td>', good_guys[i][1] ,'</td> </tr>', file = answer)
+    print('</table></body></html>', file = answer)
+    answer.close()    
+    
+
+open_files_and_process_them()
+
+
 lotr = []
 harry = []
 starwars = []
@@ -30,51 +76,31 @@ for elem in tags:
     if 'star-wars' in  elem.attrib['TagName']:
         starwars.append(elem.attrib['TagName'])
         
-posts = get_xml('Posts.xml')
+        
 lotr_users = set()
 harry_users = set()
 starwars_users = set()
-
-posts_dict = dict()
-for elem in posts:
-    posts_dict[elem.attrib['Id']] = elem
-
 for elem in posts:
     if 'OwnerUserId' in elem.attrib:
         user_id = elem.attrib['OwnerUserId']
     if elem.attrib['PostTypeId'] in ('1', '2'):
-        j = 0
         while 'ParentId' in elem.attrib:
             elem = posts_dict[elem.attrib['ParentId']]
         if elem.attrib['PostTypeId'] == '1':
-            parse_question(elem, user_id)
+            parse_question(elem, user_id)                          
+for elem in comments:
+    if 'UserId' in elem.attrib:
+        user_id = elem.attrib['UserId']
+        parse_comment(elem, user_id)
+        
             
 good_guys = []
-for elem in sorted(lotr_users):
+for elem in lotr_users:
     if elem in harry_users and (not elem in starwars_users):
-        good_guys.append(int(elem))
-
-answer = open('output.html', 'w')
-print('<html><head><title> Stats </title></head><body><h3> Here are users who participated in Lord of the Rings and Harry Potter sections, but did not participate in the Star wars section </h3><br>', file = answer) 
-print('<table border = "1px solid black">', file = answer)
-print('<th> Name </th> <th> Id </th>', file = answer)
-users = get_xml('Users.xml')
-users_dict = dict()
-for elem in users:
-    users_dict[elem.attrib['Id']] = elem
-    
+        good_guys.append(elem)
 for i in range(len(good_guys)):
-    good_guys[i] = [users_dict[str(good_guys[i])].attrib['DisplayName'], good_guys[i]]
-    
+    good_guys[i] = [users_dict[good_guys[i]].attrib['DisplayName'], good_guys[i]]
 good_guys.sort()
 
-for elem in good_guys:
-    t = elem[0]
-    elem[0] = ''
-    for letter in t:
-        if letter in string.ascii_letters:
-            elem[0] += letter
-    print('<tr> <td>', elem[0], '</td> <td>', elem[1] ,'</td> </tr>', file = answer)
 
-print('</table></body></html>', file = answer)
-answer.close()
+print_output()
